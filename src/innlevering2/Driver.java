4d45985a614d04ec2181804f79b31374eb36c4dd
Anders Hald <anderoha@stud.ntnu.no>
2016-03-14 16:07:17 +0100

    no message

--- src/innlevering2/Driver.java
+++ src/innlevering2/Driver.java
@@ -4,12 +4,10 @@ import java.awt.List;
 import java.sql.*;
 import java.sql.Date;
 import java.util.*;
-import java.util.Date;
 import java.text.*;
 
 public class Driver {
 
-<<<<<<< HEAD
 	public static void addBruker(String navn, String telefonnr, int vekt, int fødselsår) throws SQLException {
 		String addBruker = "INSERT INTO BRUKER (brukerID, navn, telefonnr, vekt, fødselsår) VALUES (default, ?, ?, ?, ?)";
 
@@ -22,10 +20,12 @@ public class Driver {
 		Statement.setInt(4, fødselsår);
 		Statement.executeUpdate();
 	}
-	
+
 	public static void currentDate() {
-		Date date = new Date();
-		String modifiedDate= new SimpleDateFormat("YYYY-MM-DD").format(date);
+		java.util.Date utilDate = new java.util.Date();
+		java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+		System.out.println("utilDate:" + utilDate);
+		System.out.println("sqlDate:" + sqlDate);
 	}
 
 	public static void addØkt(int brukerID, Date dato, int varighet, int form, int prestasjon, String notat, String lokasjon) throws SQLException {
@@ -77,29 +77,11 @@ public class Driver {
 		Statement.setString(2, øvelsesNavn);
 		Statement.setInt(3, vekt);
 		Statement.setInt(4, repetisjoner);
-		Statement.setDate(5, );
+		//		Statement.setDate(5, );
 		Statement.executeUpdate();
 	}
 
-	public static void tull() {
-		String addKategori = "INSERT INTO KATEGORI (kategoriNavn, beskrivelse) VALUES (?, ?)";
-		String addØvelse = "INSERT INTO ØVELSE (øvelsesNavn, kategoriNavn, beskrivelse) VALUES (?, ?, ?)";
-
-		ArrayList Navn = new ArrayList();
-		while (Name.next()) {
-			Navn.add(Name.getString(1));
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
-		Date date = new Date();
-		String modifiedDate= new SimpleDateFormat("YYYY-MM-DD").format(date);
-		
-		System.out.println(modifiedDate);
-		
-		
-		
-		
 		String navnValg = null;
 		int Valg = 0;
 		int brukerID = 0;
@@ -117,38 +99,10 @@ public class Driver {
 		System.out.println("Hei og velkommen! Skriv inn ditt navn: ");
 		navnValg = Scanner.nextLine();
 
-		if (!(Navn.contains(navnValg))) {
+		while (!(Navn.contains(navnValg))) {
 			System.out.println("Denne brukeren eksisterer ikke. Vil du: [1] Prøve igjen? [2] Registrere ny bruker?");
 			Valg = Scanner.nextInt();
-			if (Valg == 1) {
-				Scanner.nextLine();
-				System.out.println("Skriv inn ditt navn: ");
-				navnValg = Scanner.nextLine();
-
-				while (!(Navn.contains(navnValg))) {
-					System.out.println("Denne brukeren eksisterer ikke. Vil du: [1] Prøve igjen? [2] Registrere ny bruker?");
-					Valg = Scanner.nextInt();
-					if (Valg > 2 || Valg < 1) {
-						throw new Exception("Ikke gyldig valg [1 eller 2]. Begynn på nytt!");
-					} else if (Valg == 2) {
-						Scanner.nextLine();
-						System.out.println("Navn: ");
-						String navn = Scanner.nextLine();
-						System.out.println("Telefonnummer: ");
-						String telefonnr = Scanner.nextLine();
-						System.out.println("Vekt [i kg]: ");
-						int vekt = Scanner.nextInt();
-						System.out.println("Fødselsår: ");
-						int fødselsår = Scanner.nextInt();
-						Driver.addBruker(navn, telefonnr, vekt, fødselsår);
-						break;
-					} else if (Valg == 1) {
-						Scanner.nextLine();
-						System.out.println("Skriv inn ditt navn: ");
-						navnValg = Scanner.nextLine();
-					}
-				}
-			} else if (Valg == 2) {
+			if (Valg == 2) {
 				Scanner.nextLine();
 				System.out.println("Navn: ");
 				String navn = Scanner.nextLine();
@@ -159,30 +113,13 @@ public class Driver {
 				System.out.println("Fødselsår: ");
 				int fødselsår = Scanner.nextInt();
 				Driver.addBruker(navn, telefonnr, vekt, fødselsår);
-
-				ResultSet Login = myStatement.executeQuery("SELECT brukerID FROM BRUKER WHERE navn ='" + navnValg + "'");
-				while (Login.next()) {
-					String str = Login.getString(1);
-					brukerID = Integer.parseInt(str);
-				}
-			} else {
+				break;
+			} else if (Valg == 1) {
+				Scanner.nextLine();
+				System.out.println("Skriv inn ditt navn: ");
+				navnValg = Scanner.nextLine();
+			}else{
 				throw new Exception("Ikke gyldig valg [1 eller 2]. Begynn på nytt!");
-=======
-	public static void main(String[] args) {
-		try{
-			// 1. Get a connection to database
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/anderoha_database", "anderoha", " ");
-			
-			// 2. Get a statement
-			Statement myStmt = myConn.createStatement();
-			
-			// 3. Execute SQL query
-			ResultSet myRs = myStmt.executeQuery("SELECT * FROM bruker");
-			
-			// 4. Process the result set
-			while(myRs.next()){
-				System.out.println(myRs.getString("navn"));
->>>>>>> 61636425373538e1b483f0b6c502cc68e899befc
 			}
 		}
 		ResultSet Login = myStatement.executeQuery("SELECT brukerID FROM BRUKER WHERE navn ='" + navnValg + "'");
@@ -208,7 +145,7 @@ public class Driver {
 			int vekt = Scanner.nextInt();
 			System.out.println("Antall repetisjoner på øvelse: ");
 			int repetisjoner = Scanner.nextInt();
-			addResultat(brukerID, øvelsesNavn, vekt, repetisjoner, modifiedDate);
+			//			addResultat(brukerID, øvelsesNavn, vekt, repetisjoner, );
 			Scanner.nextLine();
 			break;
 
@@ -225,27 +162,22 @@ public class Driver {
 
 			ResultSetMetaData Resultatmetadata = PrepStatement.getMetaData();
 			int numberOfColumns = Resultatmetadata.getColumnCount();
-			
+
 			ArrayList<String[]> resultater = new ArrayList<>();
 			while( resultatSet.next()) {
-			    String[] row = new String[numberOfColumns];
-			    for( int iCol = 1; iCol <= numberOfColumns; iCol++ ){
-			        row[iCol-1] = resultatSet.getObject( iCol ).toString();
-			    }
-			    resultater.add( row );
+				String[] row = new String[numberOfColumns];
+				for( int iCol = 1; iCol <= numberOfColumns; iCol++ ){
+					row[iCol-1] = resultatSet.getObject( iCol ).toString();
+				}
+				resultater.add( row );
 			}
 			for( String[] row: resultater ){
-			    for( String s: row ){
-			        System.out.print( " " + s );
-			    }
-			    System.out.println();
+				for( String s: row ){
+					System.out.print( " " + s );
+				}
+				System.out.println();
 			}
 			break;
 		}
 	}
 }
-
-
-
-
-
